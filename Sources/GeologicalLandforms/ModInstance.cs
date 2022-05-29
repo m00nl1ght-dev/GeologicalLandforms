a5e8f69e72de187609acadf61beb2747172477ef
m00nl1ght <github@m00nl1ght.dev>
2022-05-29 18:35:28 +0200

    Version => 1.3.1

--- Sources/GeologicalLandforms/ModInstance.cs
+++ Sources/GeologicalLandforms/ModInstance.cs
@@ -5,7 +5,7 @@ namespace GeologicalLandforms;
 
 public class ModInstance : Mod
 {
-    public const string Version = "1.3.0";
+    public const string Version = "1.3.1";
 
     public static string LogPrefix => "[Geological Landforms v" + Version + "] ";
     
