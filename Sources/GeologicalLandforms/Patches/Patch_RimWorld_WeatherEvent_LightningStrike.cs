741a713c78deed10066b50c3ff524d80228b634a
m00nl1ght <github@m00nl1ght.dev>
2024-03-17 15:28:35 +0100

    Reduce frequency of lightning strikes on impassable world tiles

---
+++ Sources/GeologicalLandforms/Patches/Patch_RimWorld_WeatherEvent_LightningStrike.cs
@@ -0,0 +1,22 @@
+using HarmonyLib;
+using LunarFramework.Patching;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace GeologicalLandforms.Patches;
+
+/// <summary>
+/// Reduce frequency of lightning strikes on impassable world tiles.
+/// </summary>
+[PatchGroup("Main")]
+[HarmonyPatch(typeof(WeatherEvent_LightningStrike))]
+internal static class Patch_RimWorld_WeatherEvent_LightningStrike
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("FireEvent")]
+    private static bool TestRunInt(Map ___map)
+    {
+        return ___map.TileInfo.hilliness != Hilliness.Impassable || Rand.Value < 0.3f;
+    }
+}
