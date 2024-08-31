821b56cd57c69f2ab3c2234e9963b85385a90dc6
m00nl1ght <github@m00nl1ght.dev>
2024-09-01 00:01:58 +0200

    Fix landform data commit on preview maps

--- Sources/GeologicalLandforms/Patches/Patch_Verse_MapGenerator.cs
+++ Sources/GeologicalLandforms/Patches/Patch_Verse_MapGenerator.cs
@@ -22,12 +22,15 @@ internal static class Patch_Verse_MapGenerator
 
         Landform.Prepare(map);
 
-        var data = Find.World?.LandformData();
-        var stored = data != null && map.Tile >= 0 && data.HasData(map.Tile);
-        var indicator = data == null ? "X" : stored ? "L" : "F";
+        if (!MapPreviewGenerator.IsGeneratingOnCurrentThread)
+        {
+            var data = Find.World?.LandformData();
+            var stored = data != null && map.Tile >= 0 && data.HasData(map.Tile);
+            var indicator = data == null ? "X" : stored ? "L" : "F";
 
-        GeologicalLandformsAPI.Logger.Log($"Map generator context: {Landform.GeneratingTile} ({indicator})");
-        data?.CommitDirectly(map.Tile, new LandformData.TileData(Landform.GeneratingTile));
+            GeologicalLandformsAPI.Logger.Log($"Map generator context: {Landform.GeneratingTile} ({indicator})");
+            data?.CommitDirectly(map.Tile, new LandformData.TileData(Landform.GeneratingTile));
+        }
 
         var biomeGrid = map.BiomeGrid();
         if (biomeGrid != null)
