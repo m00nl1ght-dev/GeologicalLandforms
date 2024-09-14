77b5027d53747db9fd3a95997ea6295ada22ac27
m00nl1ght <github@m00nl1ght.dev>
2024-09-14 16:32:52 +0200

    Add compatibility patch for Map Designer river settings

--- Sources/GeologicalLandforms/Compatibility/ModCompat_MapDesigner.cs
+++ Sources/GeologicalLandforms/Compatibility/ModCompat_MapDesigner.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using GeologicalLandforms.GraphEditor;
 using HarmonyLib;
 using LunarFramework.Patching;
+using MapPreview;
+using UnityEngine;
 using Verse;
 
 namespace GeologicalLandforms.Compatibility;
@@ -16,6 +18,8 @@ internal class ModCompat_MapDesigner : ModCompat
     private FieldInfo _field_coastDir;
     private FieldInfo _field_beachTerr;
 
+    private static bool _overrideGL;
+
     protected override bool OnApply()
     {
         var modType = FindType("MapDesigner.MapDesignerMod");
@@ -25,9 +29,12 @@ internal class ModCompat_MapDesigner : ModCompat
         _field_coastDir = Require(AccessTools.Field(_settings.GetType(), "coastDir"));
         _field_beachTerr = Require(AccessTools.Field(_settings.GetType(), "beachTerr"));
 
+        GeologicalLandformsAPI.LandformEnabled.AddModifier(99, (lf, val) =>
+            val && (!_overrideGL || !lf.IsLayer || lf.LayerConfig.LayerId != "river"));
+
         GeologicalLandformsAPI.WorldTileInfoHook.AddObserver(1, info =>
         {
-            if (info.Topology.IsCoast())
+            if (_overrideGL && info.Topology.IsCoast())
             {
                 byte coastDir = (byte) _field_coastDir.GetValue(_settings);
                 if (coastDir is not (0 or > 4))
@@ -40,7 +47,7 @@ internal class ModCompat_MapDesigner : ModCompat
         NodeTerrainNaturalWater.OnCalculate += data =>
         {
             string beachTerr = (string) _field_beachTerr.GetValue(_settings);
-            if (beachTerr != null && beachTerr != "Vanilla")
+            if (_overrideGL && beachTerr != null && beachTerr != "Vanilla")
             {
                 var terrainDef = DefDatabase<TerrainDef>.GetNamedSilentFail(beachTerr);
                 if (terrainDef != null)
@@ -51,5 +58,31 @@ internal class ModCompat_MapDesigner : ModCompat
         };
 
         return true;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch("MapDesigner.UI.RiversCard", "DrawRiversCard")]
+    private static bool RiversCard_DrawRiversCard_Prefix(ref Rect rect)
+    {
+        var cbRect = rect.TopPartPixels(40f);
+        cbRect.xMin += 5f;
+        cbRect.xMax -= 20f;
+
+        var pre = _overrideGL;
+
+        Widgets.CheckboxLabeled(cbRect, "GeologicalLandforms.Integration.MapDesigner.RiverCardOverride".Translate(), ref _overrideGL);
+
+        if (pre != _overrideGL) MapPreviewAPI.NotifyWorldChanged();
+
+        rect.yMin += 40f;
+        return _overrideGL;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch("MapDesigner.UI.GeneralCard", "ResetAllSettings")]
+    private static void GeneralCard_ResetAllSettings_Postfix()
+    {
+        _overrideGL = false;
+        MapPreviewAPI.NotifyWorldChanged();
     }
 }
